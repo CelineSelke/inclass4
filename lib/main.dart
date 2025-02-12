ab3001d1c0e97c5591ced5e9875e49f57bcd1ae0
davidxia261 <david.xia261@gmail.com>
2025-02-12 15:04:23 -0500

    merged

--- lib/main.dart
+++ lib/main.dart
@@ -235,12 +235,13 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        ]
       ),
-         // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(
+        onPressed: _triggerConfetti,
+        tooltip: 'Celebrate',
+        child: const Icon(Icons.favorite),
+      ),    // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
